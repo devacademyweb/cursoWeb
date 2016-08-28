d5449ccda2ac439f2d8e772b4e14ac83dc47f10a
gitHAMP <hmendo81@gmail.com>
2016-08-28 09:45:11 -0500

    Introduccion a Java - Programacion Orientada a Objetos

---
+++ Video 1/appVideo1/src/main/java/com/devacademyweb/entity/LibroCalificaciones.java
@@ -0,0 +1,10 @@
+package com.devacademyweb.entity;
+
+
+public class LibroCalificaciones {
+
+	public void mostrarMensaje(){
+		System.out.println("Bienvenidos al curso");
+	}
+	
+}
---
+++ Video 1/appVideo1/src/main/java/com/devacademyweb/view/PruebaLibroCalificaciones.java
@@ -0,0 +1,20 @@
+package com.devacademyweb.view;
+
+import com.devacademyweb.entity.LibroCalificaciones;
+
+//Crear un objeto LibroCalificaciones y llama a su metodo
+public class PruebaLibroCalificaciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Crea un objeto LibroCalificaciones.
+		LibroCalificaciones miLibroCalificaciones1=new LibroCalificaciones();
+		LibroCalificaciones miLibroCalificaciones2=new LibroCalificaciones();
+		
+		//llama al metodo mostrarMensaje de miLibroCalificaciones
+		miLibroCalificaciones1.mostrarMensaje();
+		miLibroCalificaciones2.mostrarMensaje();
+		
+	}
+
+}
