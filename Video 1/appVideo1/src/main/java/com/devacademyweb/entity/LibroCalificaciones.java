af4bd5cb2ab441f1a4b29e6c1c76418b4cf47472
gitHAMP <hmendo81@gmail.com>
2016-08-28 10:09:41 -0500

    Variables de instancia, metodos establecer y metodos obtener

--- Video 1/appVideo1/src/main/java/com/devacademyweb/entity/LibroCalificaciones.java
+++ Video 1/appVideo1/src/main/java/com/devacademyweb/entity/LibroCalificaciones.java
@@ -1,10 +1,23 @@
 package com.devacademyweb.entity;
 
-
 public class LibroCalificaciones {
+	private String nombreDelCurso;
 
-	public void mostrarMensaje(){
-		System.out.println("Bienvenidos al curso");
+	public LibroCalificaciones() {
+		// TODO Auto-generated constructor stub
+		this.nombreDelCurso="Curso Inicial";
 	}
 	
+	public String getNombreDelCurso() {
+		return nombreDelCurso;
+	}
+
+	public void setNombreDelCurso(String nombreDelCurso) {
+		this.nombreDelCurso = nombreDelCurso;
+	}
+
+	public void mostrarMensaje() {
+		System.out.printf("Bienvenido al libro de calificaciones para\n%s!\n", this.getNombreDelCurso());
+	}
+
 }
--- Video 1/appVideo1/src/main/java/com/devacademyweb/view/PruebaLibroCalificaciones.java
+++ Video 1/appVideo1/src/main/java/com/devacademyweb/view/PruebaLibroCalificaciones.java
@@ -1,5 +1,7 @@
 package com.devacademyweb.view;
 
+import java.util.Scanner;
+
 import com.devacademyweb.entity.LibroCalificaciones;
 
 //Crear un objeto LibroCalificaciones y llama a su metodo
@@ -7,13 +9,26 @@ public class PruebaLibroCalificaciones {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Crear un objeto Scanner para obtener la entrada de la ventana de comandos
+		Scanner entrada=new Scanner(System.in);
+		String nombreDelCurso;
+		
 		//Crea un objeto LibroCalificaciones.
 		LibroCalificaciones miLibroCalificaciones1=new LibroCalificaciones();
-		LibroCalificaciones miLibroCalificaciones2=new LibroCalificaciones();
+		
+		System.out.printf("El nombre inicial del curso es:%s\n\n", miLibroCalificaciones1.getNombreDelCurso());
+				
+		
+		//pide y recibe el nombre del curso como entrada.
+		System.out.println("Escriba el nombre del curso:");
+		nombreDelCurso=entrada.nextLine();//lee una linea de texto
+		miLibroCalificaciones1.setNombreDelCurso(nombreDelCurso);
+		System.out.println();
 		
 		//llama al metodo mostrarMensaje de miLibroCalificaciones
 		miLibroCalificaciones1.mostrarMensaje();
-		miLibroCalificaciones2.mostrarMensaje();
+		
 		
 	}
 
